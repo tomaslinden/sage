1f0494a173be4ec7fbdb6a86f470adf7b20a1954
Tomas Linden <tomaslinden24@gmail.com>
2018-11-02 17:57:23 +0200

    Add initial support for rendering facets around review center

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CircularGraph :reviewData="reviewData" :graphData="graphData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CircularGraph from './components/CircularGraph.vue'
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+var dimensions = [width, height];
+var minDimension = Math.min(width, height)
+var centerCoords = {x: dimensions[0]/2, y: dimensions[0]/2}
+console.log(minDimension)
+
+var graphData = [];
+
+let reviewData = { 
+  topic: 'Topic',
+  framework: {
+    facets: [
+      { handle: 'big' },
+      { handle: 'solvable' },
+      { handle: 'neglected' },
+    ]
+  }
+}
+
+// var angle = 0;
+// var angleIncrement = 360/reviewData.framework.facets.length;
+// var angleOffset = (270 - angleIncrement/2)%360;
+// for (let value of reviewData.framework.facets) {
+//   var configArc = {
+//     x: centerCoords.x,
+//     y: centerCoords.y,
+//     innerRadius: minDimension/4,
+//     outerRadius: minDimension/3,
+//     angle: angleIncrement,
+//     stroke: 'black',
+//     strokeWidth: 2,
+//     rotationDeg: angleOffset + angle
+//   };
+//   graphData.push({
+//     handle: value.handle,
+//     configArc
+//   });
+//   angle += angleIncrement;
+// }
+
+var angle = 0;
+var facetAngleSize = 360/reviewData.framework.facets.length;
+var initialAngleOffset = (270 - facetAngleSize/2)%360;
+for (let value of reviewData.framework.facets) {
+  var configArc = {
+    x: centerCoords.x,
+    y: centerCoords.y,
+    innerRadius: minDimension/4,
+    outerRadius: minDimension/3,
+    angle: facetAngleSize,
+    stroke: 'black',
+    strokeWidth: 2,
+    rotationDeg: initialAngleOffset + angle
+  };
+  graphData.push({
+    handle: value.handle,
+    configArc
+  });
+  angle += facetAngleSize;
+}
+
+
+graphData = JSON.parse(JSON.stringify(graphData));
+
+console.log('graphData', JSON.parse(JSON.stringify(graphData)));
+
+
+const testMsg = "Welcome to Your Vue.js App"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CircularGraph,
+  },
+  data: () => {
+    return {
+      graphData: graphData,
+      reviewData: reviewData
+    };
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
+
+polygon {
+    fill: #42b983;
+    opacity: .75;
+}
+
+circle {
+    fill: transparent;
+    stroke: #999;
+}
+
+text {
+    font-family: Helvetica Neue, Arial, sans-serif;
+    font-size: 10px;
+    fill: #666;
+}
+
+label {
+    display: inline-block;
+    margin-left: 10px;
+    width: 20px;
+}
+
+#raw {
+    position: absolute;
+    top: 0;
+    left: 300px;
+}
+
 </style>
---
+++ src/components/CircularGraph.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="hello">
+    <div style="display: flex; justify-content: center;">
+      <v-stage ref="stage" :config="configKonva">
+        <v-layer ref="layer">
+          <v-circle :config="configCircle"></v-circle>
+        </v-layer>
+        <v-layer :ref="value.handle" :key="value.handle" v-for="value in graphData">
+          <v-arc :config="value.configArc" v-if="value.type === 'facet'"></v-arc>
+        </v-layer>
+      </v-stage>
+    </div>
+  </div>
+</template>
+
+<script>
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+var dimensions = [width, height];
+var minDimension = Math.min(width, height)
+var centerCoords = {x: dimensions[0]/2, y: dimensions[0]/2}
+
+export default {
+  name: 'CircularGraph',
+  props: {
+    reviewData: Object,
+    graphData: Array
+  },
+  data: () => {
+    return {
+      configKonva: {
+        // width: width,
+        // height: height
+        width: dimensions[0],
+        height: dimensions[1]
+      },
+      configCircle: {
+        x: centerCoords.x,
+        y: centerCoords.y,
+        radius: minDimension/3,
+        // fill: "red",
+        stroke: "black",
+        strokeWidth: 2
+      },
+      configArc: {
+        x: centerCoords.x,
+        y: centerCoords.y,
+        innerRadius: minDimension/4,
+        outerRadius: minDimension/3,
+        angle: 60,
+        // fill: 'yellow',
+        stroke: 'black',
+        strokeWidth: 2
+      }
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
